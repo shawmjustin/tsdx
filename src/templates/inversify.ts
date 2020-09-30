37d0fea6765436bea52a5717c3c963a435240e7a
Justin Shaw <justin.shaw@voicefoundry.com>
2020-09-29 22:29:45 -0500

    refactor: deps

--- src/templates/inversify.ts
+++ src/templates/inversify.ts
@@ -2,15 +2,16 @@ import { Template } from './template';
 
 const basicTemplate: Template = {
   name: 'inversify',
-  dependencies: [
-    '@vf-team5/logger',
-    '@vf-team5/types',
-    'aws-sdk',
-    'inversify',
-    'inversify-inject-decorators',
-    'reflect-metadata',
-  ],
+  dependencies: [],
   packageJson: {
+    dependencies: {
+      '@vf-team5/logger': '^0.0.47',
+      '@vf-team5/types': '^0.0.47',
+      'aws-sdk': '^2.763.0',
+      inversify: '^5.0.1',
+      'inversify-inject-decorators': '^3.1.0',
+      'reflect-metadata': '^0.1.13',
+    },
     // name: safeName,
     version: '0.1.0',
     license: 'MIT',
